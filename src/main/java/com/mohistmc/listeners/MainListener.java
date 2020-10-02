c1fc84fdd736146c50953322eb2c35a0197c4bfe
lliioollcn <lliiooll@126.com>
2020-10-02 22:44:18 +0800

    Update MainListener.java

--- src/main/java/com/mohistmc/listeners/MainListener.java
+++ src/main/java/com/mohistmc/listeners/MainListener.java
@@ -5,6 +5,7 @@ import com.mohistmc.cmds.manager.CommandManager;
 import com.mohistmc.utils.HasteUtils;
 import com.mohistmc.utils.LogUtil;
 import kotlin.coroutines.CoroutineContext;
+import net.mamoe.mirai.contact.MemberPermission;
 import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.ListeningStatus;
 import net.mamoe.mirai.event.SimpleListenerHost;
@@ -18,7 +19,7 @@ public class MainListener extends SimpleListenerHost {
     @EventHandler
     public ListeningStatus onGroupMessage(GroupMessageEvent event) throws IOException {
         String content = event.getMessage().contentToString();
-        if (content.length() >= 100) {
+        if ((content.length() >= 100) && (event.getSender().getPermission() == MemberPermission.MEMBER)) {
             if (!(event.getMessage() instanceof RichMessage)) {
                 MiraiMBot.bot.recall(event.getMessage());
                 event.getGroup().sendMessage("您的消息已经被转移到此：" + HasteUtils.paste(content));
